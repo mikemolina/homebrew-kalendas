a91e551c935f38383d2f9eed85962cf167828089
Mike Molina <mmolina.unphysics@gmail.com>
2014-09-14 00:52:48 -0500

    Add installation for version devel

--- kalendas.rb
+++ kalendas.rb
@@ -2,16 +2,40 @@ require "formula"
 
 class Kalendas < Formula
   homepage "https://github.com/mikemolina/kalendas"
-  url "https://launchpad.net/kalendas/trunk/1.0.0/+download/kalendas-1.0.0.tar.gz"
-  sha1 "94879eb4e1537cfb0899a074764c7caca91f1514"
+  url "https://launchpad.net/kalendas/trunk/1.0.1/+download/kalendas-1.0.1.tar.gz"
+  sha1 "f3d54eadadba21a238a6ede7a7830e8d797b0937"
 
-  # gettext, Dependencia en duda?
-  # depends_on "gettext"
+  head do
+    url "https://github.com/mikemolina/kalendas.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+    depends_on "texinfo" => :build
+  end
+
+  option "enable-charset-latin1", "Build with charset latin1"
+
   depends_on "Locale::TextDomain" => :perl
+  depends_on "gettext" if build.include? "enable-charset-latin1"
+  depends_on "texinfo" if build.include? "enable-charset-latin1"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./autogen.sh" if build.head?
+
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+    ]
+
+    args << "--enable-charset=latin1" if build.include? "enable-charset-latin1"
+
+    system "./configure", *args
     system "make"
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/kalendas"
   end
 end
